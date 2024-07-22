2e16223633456e0d21c6fea358278e177ffb1db2
mughal-71 <abdullahmughal71.com@gmail.com>
2024-07-22 22:38:49 +0500

    this is a variable task

---
+++ if else.js
@@ -0,0 +1,34 @@
+console.log("practice")
+fullName = "muhammad abdullah"
+console.log(fullName);
+age = 24;
+console.log(age)
+fullName = "aftab hussain";
+age = 50;
+gender = "male"
+console.log(age)
+console.log(gender)
+console.log(fullName)
+var price = 299.9;
+var cgpa = 2.66;
+console.log(cgpa)
+console.log(price)
+{
+    let a= 5;
+    console.log(a)
+}
+const student = {
+    fullName : "muhamad abdullah",
+    age : 24,
+    cgpa : 2.66,
+    isPass : true, 
+};
+student ["age"] = student["age"] + 1;
+console.log(student["age"]);
+
+student["cgpa"] = student["cgpa"] + 1.11;
+
+
+student["isPass"] = student["isPass"] + fail;
+console.log(isPass[isPass]);
+
